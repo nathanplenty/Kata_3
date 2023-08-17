06542cc74122e423f22bf733d163ee87f90687cb
nathanplenty <nathan.roskamp@plentysystems.com>
2023-08-17 13:02:48 +0200

    Update kata_3.go
    Working Prototype

--- kata_3.go
+++ kata_3.go
@@ -1,127 +1,135 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	// "strings"
 )
 
 func isValid(s string) bool {
 	b := false
-	bl := false
-	br1 := '0'
-	br2 := '0'
 	i := 0
-	// Pseudo Code
-	// Get string -> "test(number)index{}end"
-	fmt.Println("Input:", s)
-	// Search for opener bracket -> "test'('number)index{}end"
-	// Save bracket -> br = "("
-	g1 := func() {
-		bl = false
-	out:
-		for i = 0; i < len(s); i++ {
+	p := 0
+	type bracket struct {
+		x int
+		y byte
+	}
+	bro := bracket{0, '0'}
+	sis := bracket{0, '0'}
+	// 1. Get string
+	// fmt.Println("Input:", s)
+	// 2. Is string bigger than 0
+	// 2.1. If false -> kill funcion, return bool
+	for {
+		if len(s) == 0 {
+			return b
+		}
+		// 2.2. If true -> continue
+		// 3. Search for bracket
+		i = 0
+		g1 := func() bool {
+			b = false
+			// fmt.Printf("%T", s)
+			for ; i < len(s); i++ {
+				p = i
+				b = bytes.ContainsAny([]byte(s), "()[]{}")
+				if b == true {
+					return b
+				}
+			}
+			return b
+		}
+		b = g1()
+		if b == false {
+			return b
+		}
+		// 4. Is bracket opener -> True / False
+		// 4.1. If true -> Save bracket 1 and position
+		o1 := func() bool {
+			b = false
 			switch {
-			case s[i] == '(':
-				bl = true
-				br1 = '('
-				break out
-			case s[i] == '{':
-				bl = true
-				br1 = '{'
-				break out
-			case s[i] == '[':
-				bl = true
-				br1 = '['
-				break out
-			default:
-				continue
+			case s[p] == '(':
+				bro = bracket{p, '('}
+				b = true
+			case s[p] == '{':
+				bro = bracket{p, '{'}
+				b = true
+			case s[p] == '[':
+				bro = bracket{p, '['}
+				b = true
 			}
+			return b
 		}
-	}
-	g1()
-	// If no opener bracket found -> False
-	if bl == false {
-		return false
-	}
-
-	// Delete front of open bracket -> "(number)index{}end"
-	m := i + 1
-	// fmt.Println("Bracket:", string(br1))
-	for i = 0; i < m; i++ {
-		s = s[1:]
-	}
-	// fmt.Println("Trimed:", s)
-	// Search for next bracket -> (number')'index{}end"
-	i = 0
-	g2 := func() {
-		bl = false
-	out2:
-		for i = 0; i < len(s); i++ {
+		b = o1()
+		// 4.2. If false -> kill function, return false
+		if b == false {
+			return b
+		}
+		// 5. Search for bracket after position of bracket 1
+		l := 0
+		for {
+			// fmt.Println("Loop round:", l)
+			l = l + 1
+			i = p + 1
+			b = g1()
+			if b == false {
+				return b
+			}
+			// 6. Is bracket opener -> True / False
+			b = o1()
+			// 6.1. If false -> GoTo Step 7.
+			if b == false {
+				break
+			}
+			// 6.2. If true -> overwrite bracket 1 and position -> GoTo Step 5.
+		}
+		// 7. Is bracket closer to bracket 1
+		o2 := func() bool {
+			b = false
 			switch {
-			case s[i] == ')':
-				bl = true
-				br2 = ')'
-				break out2
-			case s[i] == '}':
-				bl = true
-				br2 = '}'
-				break out2
-			case s[i] == ']':
-				bl = true
-				br2 = ']'
-				break out2
-			default:
-				continue
+			case s[p] == ')':
+				sis = bracket{p, '('}
+				b = true
+			case s[p] == '}':
+				sis = bracket{p, '{'}
+				b = true
+			case s[p] == ']':
+				sis = bracket{p, '['}
+				b = true
 			}
+			return b
+		}
+		b = o2()
+		// 7.1. If true -> Delete both, go back so step 1.
+		del := func() {
+			brocopy := s
+			brocopy = brocopy[:(bro.x)]
+			// fmt.Println(brocopy)
+			siscopy := s
+			siscopy = siscopy[(sis.x + 1):]
+			// fmt.Println(siscopy)
+			s = brocopy + siscopy
+		}
+		b = false
+		if bro.y == sis.y {
+			b = true
+			del()
+		}
+		// 7.2. If false -> kill function, return false
+		if bro.y != sis.y {
+			b = false
+			break
 		}
 	}
-	g2()
-	// if no bracket found -> False
-	if bl == false {
-		return false
-	}
-	m = i + 1
-	// fmt.Println("Bracket:", string(br2))
-	// Is bracket valid to saved bracket? -> True / False
-	bl = false
-	// fmt.Println("Bracket-Brothers:", string(br1), string(br2))
-	bb := func() {
-		b = true
-		switch {
-		case br1 == '(':
-			if br1 == br2-1 {
-				bl = true
-				// fmt.Println("()", bl)
-			}
-		case br1 == '{':
-			if br1 == br2-2 {
-				bl = true
-				// fmt.Println("{}", bl)
-			}
-		case br1 == '[':
-			if br1 == br2-2 {
-				bl = true
-				// fmt.Println("[]", bl)
-			}
-		default:
-			//should never happen
-			// fmt.Println("WHY IS THIS?!")
-		}
-	}
-	bb()
-	// Delete front of closer bracket including bracket-> "index{}end"
-	for i = 0; i < m; i++ {
-		s = s[1:]
-	}
-	// fmt.Println("Trimed:", s)
-	// Repeat
-	b = isValid(s)
 	return b
 }
 
 func main() {
-	fmt.Println(isValid("()"))
-	fmt.Println(isValid("()[]{}"))
-	fmt.Println(isValid("(]"))
-	fmt.Println(isValid("([)]"))
-	fmt.Println(isValid("{[]}"))
+	fmt.Println("START")
+	fmt.Println("Is Valid?:", isValid("()"))
+	fmt.Println("Is Valid?:", isValid("()[]{}"))
+	fmt.Println("Is Valid?:", isValid("(])"))
+	fmt.Println("Is Valid?:", isValid("([)]"))
+	fmt.Println("Is Valid?:", isValid("{[]}"))
+	fmt.Println("STOP")
 }
