dbf5314e6822205582e7aa5bb6a02c68ebbe40d1
nathanplenty <nathan.roskamp@plentysystems.com>
2023-08-23 15:40:27 +0200

    Update kata_3.go
    - better naming

--- kata_3.go
+++ kata_3.go
@@ -5,26 +5,26 @@ import (
 	"fmt"
 )
 
-func isValid(s string) bool {
+func isValid(stringIn string) bool {
 	// Initialize stack as placeholder for brackets
 	type Stack struct {
 		Chars []byte
 	}
 	stack := Stack{}
 	// Initialize current character for current bracket
-	var current byte = '0'
-	// Get each character as i
-	for i := 0; i < len(s); i++ {
-		current = byte(s[i])
+	var currentCharacter byte = '0'
+	// Get each character as char
+	for char := 0; char < len(stringIn); char++ {
+		currentCharacter = byte(stringIn[char])
 		// Check if character can be used
 		switch {
 		// If opener bracket, append it
-		case true == bytes.ContainsAny([]byte(string(current)), "([{"):
-			stack.Chars = append(stack.Chars, current)
+		case true == bytes.ContainsAny([]byte(string(currentCharacter)), "([{"):
+			stack.Chars = append(stack.Chars, currentCharacter)
 		// If correct closer bracket, remove it and its opener
-		case current-1 == stack.Chars[(len(stack.Chars))-1]:
+		case currentCharacter-1 == stack.Chars[(len(stack.Chars))-1]:
 			stack.Chars = stack.Chars[:(len(stack.Chars))-1]
-		case current-2 == stack.Chars[(len(stack.Chars))-1]:
+		case currentCharacter-2 == stack.Chars[(len(stack.Chars))-1]:
 			stack.Chars = stack.Chars[:(len(stack.Chars))-1]
 		default:
 			return false
